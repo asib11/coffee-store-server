07a3816b88cd0ea16427c32de393a0de88458013
asib ahmed <ahmedasib1@gmail.com>
2023-12-07 00:16:42 +0600

    add data in database

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhelgf3.mongodb.net/?retryWrites=true&w=majority1`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhelgf3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,12 +24,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+    app.post('/coffee', async(req, res) =>{
+        const newcoffee = req.body;
+        console.log(newcoffee);
+        const result = await coffeeCollection.insertOne(newcoffee);
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
