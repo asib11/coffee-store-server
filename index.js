f50b88a5e1939e37d188969274928850d20a4874
asib ahmed <ahmedasib1@gmail.com>
2023-12-07 22:44:01 +0600

    silly missing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
@@ -39,7 +39,7 @@ async function run() {
         res.send(result);
     })
 
-    app.delete('coffee/:id', async(req, res)=>{
+    app.delete('/coffee/:id', async(req, res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await coffeeCollection.deleteOne(query);
